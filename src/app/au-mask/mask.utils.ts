f50f81836c45f7d843abfaef2b151486bf3f79f2
Stephanie Smith <stephanie.lauren.design@gmail.com>
2019-05-02 08:30:49 -0700

    starter files

---
+++ src/app/au-mask/mask.utils.ts
@@ -0,0 +1,15 @@
+
+
+
+export const  TAB = 9,
+    LEFT_ARROW =	37,
+    RIGHT_ARROW = 39,
+    BACKSPACE = 8,
+    DELETE = 46;
+
+export const SPECIAL_CHARACTERS = [" ", "/", "(", ")", "+", "\/", "-"];
+
+
+
+
+
